7d4ca5cefa50e48e735a897c7c982c279279645b
Jhon Jario <152086681+JhonJMD@users.noreply.github.com>
2023-12-15 06:18:05 -0500

    Concatenacion cambiada

--- ahorcado.py
+++ ahorcado.py
@@ -1,38 +1,26 @@
 import os
 os.system('cls')
-# Palabra a adivinar
-palabra = "python"
-# Estado actual de la palabra a mostrar al jugador
+palabra = "ahorcado"
 result = "_" * len(palabra)
-# Número de intentos restantes
 intentos_restantes = 3
 print ("Bienvenido al juego del Ahorcado")
-# Bucle principal del juego
 while intentos_restantes > 0 and result != palabra:
-  print("Palabra:", result)
-  print("Intentos restantes:", intentos_restantes)
-  # Solicitar al jugador ingresar una letra o la palabra completa
-  print ("Ingresa una letra o la palabra completa: ")
-  entrada = input().lower()
+  print(f"Palabra: {result}")
+  print(f"Intentos restantes: {intentos_restantes}")
+  entrada = input("Ingresa una letra o la palabra completa: ").lower()
   if len(entrada) == 1:
-    # El jugador ingresó una letra
     if entrada in palabra:
-      # La letra ingresada está en la palabra
       for i in range(len(palabra)):
         if palabra[i] == entrada:
           result = result[:i] + entrada + result[i + 1:]
-          #result = result.replace(result, entrada+(" _ "*(len(palabra))))
     else:
-      # La letra ingresada no está en la palabra
       intentos_restantes -= 1
   else:
-    # El jugador ingresó la palabra completa
     if entrada == palabra:
       result = palabra
     else:
       intentos_restantes -= 1
-# Mostrar resultado del juego
 if result == palabra:
-  print("¡Felicidades! Has adivinado la palabra:", palabra)
+  print(f"¡Felicidades! Has adivinado la palabra: {palabra}")
 else:
-  print("Has perdido. La palabra era:", palabra)
+  print(f"Has perdido. La palabra era: {palabra}")
